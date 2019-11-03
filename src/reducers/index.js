6b6c61dfa7a27779838dd8f21ed077bbce6e3d83
Saqib Nazir <saqib_9090@yahoo.com>
2019-11-03 01:20:19 +0100

    used the store  in react

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 import counterReducer from './counter';
-import LoggedReducer from './isLogged';
-import { combineReducers } from 'redux';
 import loggedReducer from './isLogged';
+import { combineReducers } from 'redux';
 
 const allReducers = combineReducers({
   counter: counterReducer,
   isLogged: loggedReducer
 });
+export default allReducers;
